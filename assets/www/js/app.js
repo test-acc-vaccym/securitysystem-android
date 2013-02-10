3d3538d58a510aefedda2f159ae0acb4e1ba49bd
Jake Klingler <thearrow3456@gmail.com>
2013-02-09 21:08:15 -0500

    edit sensor functionality and start sensor details

--- assets/www/js/app.js
+++ assets/www/js/app.js
@@ -1,4 +1,5 @@
 var apiURL = 'http://securitysystem.herokuapp.com';
+var oldKey = "";
 
 //set some defaults before the app loads
 $(document).on("mobileinit", function () {
@@ -10,18 +11,36 @@ $(document).on("mobileinit", function () {
 
 //set sensor information on edit popup
 $(document).on('vclick', '.edit-gear', function () {
+    var key = $(this).parent().find('p').text();
     var edit = $('#edit');
-    edit.find('h3').text('Edit ' + $(this).parent().find('h3').text());
-    edit.find('p:first').text($(this).parent().find('p').text());
+    edit.find('#edit-sensor-name').val($(this).parent().find('h3').text());
+    edit.find('#edit-sensor-key').val(key);
+    oldKey = key;
 });
 
+//set sensor information on view popup
+$(document).on('vclick', '.view-button', function(){
+
+});
+
+//edit sensor
+$(document).on('submit','#edit-sensor-form',function(){
+    var name = $(this).find('#edit-sensor-name').val();
+    var key = $(this).find('#edit-sensor-key').val();
+    $.ajax({
+        type: "PUT",
+        url: apiURL+"/sensors/"+oldKey,
+        data: {sensor: { name:name, sensor_id:key }}
+    }).done(function() {
+            reloadSensorList();
+    });
+});
 
 //delete sensor
 $(document).on('vclick', '#delete-sensor-button', function () {
-    var id = $(this).parent().parent().find('p:first').text();
     $.ajax({
         type: "DELETE",
-        url: apiURL+"/sensors/"+id
+        url: apiURL+"/sensors/"+oldKey
     }).done(function() {
             reloadSensorList();
         });
@@ -35,15 +54,12 @@ $(document).on('submit','#add-sensor-form',function(){
     try{
         pushID = window.getAPID.getAPID();
     } catch(err){
-        alert("Couldn't get APID! "+err);
+        alert("Couldn't get APID!");
+        pushID = " ";
     }
 
     var name = $('#new-sensor-name').val();
     var id = $('#new-sensor-key').val();
-    if(pushID === null) {
-        pushID="";
-    }
-
 
     $.ajax({
         type: "POST",
@@ -60,34 +76,40 @@ $(document).on('vclick','#refresh-button',function(){
     reloadSensorList();
 });
 
-//reload the sensor list
 $(document).on('pagebeforeshow', '#sensorpage',function () {
     reloadSensorList();
 });
 
 function reloadSensorList(){
-    $.getJSON(apiURL+'/sensors.json', function(sensors){
-        //populate the listview with items from sensors array
-        var sensorlist = $('#sensorlist');
-        sensorlist.empty();
-        for (var i in sensors) {
-            if (sensors.hasOwnProperty(i)) {
-                var newItem = $('<li/>');
-                var inner = $('<a/>', {'href': '#'});
-                if(sensors[i].sensor.tripped){
-                    inner.append($('<img/>', {'src': 'img/danger.png'}));
+
+    try{
+        $.getJSON(apiURL+'/sensors.json', function(sensors){
+            //populate the listview with items from sensors array
+            var sensorlist = $('#sensorlist');
+            sensorlist.empty();
+            for (var i in sensors) {
+                if (sensors.hasOwnProperty(i)) {
+                    var newItem = $('<li/>');
+                    var inner = $('<a/>', {'class': 'view-button' ,'href': '#view', 'data-rel': 'popup', 'data-transition': 'pop', 'data-position-to': 'window'});
+                    if(sensors[i].sensor.tripped){
+                        inner.append($('<img/>', {'src': 'img/danger.png'}));
+                    }
+                    else {
+                        inner.append($('<img/>', {'src': 'img/check.png'}));
+                    }
+                    inner.append($('<h3/>', {'text': sensors[i].sensor.name }));
+                    inner.append($('<p/>', {'text': sensors[i].sensor.sensor_id}));
+                    newItem.append(inner);
+                    newItem.append($('<a/>', {'class': 'edit-gear', 'href': '#edit', 'data-rel': 'popup', 'data-transition': 'pop', 'data-position-to': 'window'}));
+                    sensorlist.append(newItem);
                 }
-                else {
-                    inner.append($('<img/>', {'src': 'img/check.png'}));
-                }
-                inner.append($('<h3/>', {'text': sensors[i].sensor.name }));
-                inner.append($('<p/>', {'text': sensors[i].sensor.sensor_id}));
-                newItem.append(inner);
-                newItem.append($('<a/>', {'class': 'edit-gear', 'href': '#edit', 'data-rel': 'popup', 'data-transition': 'pop', 'data-position-to': 'window'}));
-                sensorlist.append(newItem);
             }
-        }
-        sensorlist.listview('refresh');
-    });
+            sensorlist.listview('refresh');
+        });
+    }
+    catch(error){
+        alert("Couldn't get the sensor list!");
+    }
+
 
 }
